833151a33a773cfc5954fe4cc98a761e1213b863
Blue Putty <65861156+blue-putty@users.noreply.github.com>
2021-08-03 21:43:09 -0400

    add secrets to build

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
 const path = require('path');
 require('dotenv').config();
 const webpack = require('webpack');
-const manifest = require('./src/manifest.json')
+const CopyPlugin = require('copy-webpack-plugin')
+
+function modify(buffer) {
+    // copy-webpack-plugin passes a buffer
+    const manifest = JSON.parse(buffer.toString());
+
+    // make modifications
+    manifest.key = process.env.KEY;
+    manifest.oauth2.client_id = process.env.CLIENT_ID;
+
+    // pretty print to JSON with two spaces
+    const manifest_JSON = JSON.stringify(manifest, null, 2);
+    return manifest_JSON;
+}
 
 module.exports = {
     mode: 'production',
@@ -9,14 +22,21 @@ module.exports = {
         serviceWorker: './src/serviceWorker.js',
     },
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, './dist'),
         filename: '[name].js',
+        clean: true,
     },
-    
     plugins: [
-        new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
-        'process.env.CLIENT_ID': JSON.stringify(process.env.CLIENT_ID),
-        'process.env.KEY': JSON.stringify(process.env.KEY)
-    }),]
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "./src/manifest.json",
+                    to: "manifest.json",
+                    transform(content, path) {
+                        return modify(content)
+                    }
+                }
+            ]
+        })
+    ]
 };
