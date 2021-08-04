c38c79637004b6f67b14c454cc32f3b5b8045739
Blue Putty <65861156+blue-putty@users.noreply.github.com>
2021-08-04 15:20:03 -0400

    add live reloading to dev build

--- webpack.dev.js
+++ webpack.dev.js
@@ -19,6 +19,9 @@ function modify(buffer) {
 
 module.exports = merge(common, {
     mode: 'development',
+    entry: {
+        serviceWorker: ['./src/serviceWorker.js', './scripts/liveReload.js'],
+    },
     plugins: [
         new CopyPlugin({
             patterns: [
