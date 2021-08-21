0bcb5eb7ccd4667977556cfbf5784f52d6da3efc
blue-putty <65861156+blue-putty@users.noreply.github.com>
2021-08-21 13:50:12 -0400

    copy webpack config from master

---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/js/popup.js',
+  },
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: '[name].js',
+    clean: true,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: './src/64.png',
+          to: '64.png',
+        },
+        {
+          from: './src/popup.html',
+          to: 'popup.html',
+        },
+        {
+          from: './src/js/',
+          to: '/js/',
+        },
+        {
+          from: './src/css/popup.css',
+          to: 'css/popup.css',
+        },
+      ],
+    }),
+  ],
+};
