259146e45be12496e1b6ab9aac6597b040d1869e
blue-putty <65861156+blue-putty@users.noreply.github.com>
2021-08-21 12:59:17 -0400

    formatting

--- src/js/actions.js
+++ src/js/actions.js
@@ -1,16 +1,17 @@
 function popout(tab) {
-    // console.log(tab);
-    // chrome.tabs.create({url: tab.url, windowType: 'popup'})
-    chrome.scripting.executeScript(
-        {
-            target: { tabId: tab.id },
-            function: action,
-        },
-        () => { });
-    function action() {
-        window.open(location.href, "detab", "toolbar=0");
-        window.close();
-    }
+  // console.log(tab);
+  // chrome.tabs.create({url: tab.url, windowType: 'popup'})
+  chrome.scripting.executeScript(
+    {
+      target: { tabId: tab.id },
+      function: action,
+    },
+    () => {}
+  );
+  function action() {
+    window.open(location.href, 'detab', 'toolbar=0');
+    window.close();
+  }
 }
 
 export default { popout };
